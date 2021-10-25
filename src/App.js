1d327d1d883cb8cd235c83f78041e240b4dbcb36
Melissa McGuire <maggiemadison1221@gmail.com>
2021-10-24 22:33:51 -0500

    use const w/ lambda instead of function key word

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 
- const App = () =>
- {
-  return 
-  (
+ const App = () => {
+  return (
     <>
-      
+      <h1>Let's do some math !</h1>
+      <h2>The sum of number 1 and number 2 = SUM</h2>
+
+      <h2>The sum of number 1 and number 2 = SUM</h2>
+
+      <h2>The sum of number 1 and number 2 = SUM</h2>
+
+      <h2>The difference of number 1 and number 2 = DIFFERENCE</h2>
+
+      <h2>The difference of number 1 and number 2 = DIFFERENCE</h2>
+
+      <h2>The difference of number 1 and number 2 = DIFFERENCE</h2>
+
+      <h2>The product of number 1 and number 2 = PRODUCT</h2>
+
+      <h2>The product of number 1 and number 2 = PRODUCT</h2>
+
+      <h2>The product of number 1 and number 2 = PRODUCT</h2>
+
+      <h2>The quotient of number 1 and number 2 = QUOTIENT</h2>
+
+      <h2>The quotient of number 1 and number 2 = QUOTIENT</h2>
+
+      <h2>The quotient of number 1 and number 2 = QUOTIENT</h2>
     </>
   );
 }

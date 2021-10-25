3a7bc5412b2fe488cde1ea10af1c34f38651ed88
Melissa McGuire <maggiemadison1221@gmail.com>
2021-10-24 22:46:39 -0500

    adding a math component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import './App.css';
+import Mathematics from './Mathematics';
 
  const App = () => {
   return (
-    <>
-      <h1>Let's do some math !</h1>
-      <h2>The sum of number 1 and number 2 = SUM</h2>
-
-      <h2>The sum of number 1 and number 2 = SUM</h2>
-
-      <h2>The sum of number 1 and number 2 = SUM</h2>
-
-      <h2>The difference of number 1 and number 2 = DIFFERENCE</h2>
-
-      <h2>The difference of number 1 and number 2 = DIFFERENCE</h2>
-
-      <h2>The difference of number 1 and number 2 = DIFFERENCE</h2>
-
-      <h2>The product of number 1 and number 2 = PRODUCT</h2>
-
-      <h2>The product of number 1 and number 2 = PRODUCT</h2>
-
-      <h2>The product of number 1 and number 2 = PRODUCT</h2>
-
-      <h2>The quotient of number 1 and number 2 = QUOTIENT</h2>
-
-      <h2>The quotient of number 1 and number 2 = QUOTIENT</h2>
-
-      <h2>The quotient of number 1 and number 2 = QUOTIENT</h2>
-    </>
+    <Mathematics />
   );
 }
 
---
+++ src/Mathematics.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Mathematics = () => {
+    return (
+        <>
+      <h1>Let's do some math !</h1>
+      <h2>The sum of number 1 and number 2 = SUM</h2>
+
+      <h2>The sum of number 1 and number 2 = SUM</h2>
+
+      <h2>The sum of number 1 and number 2 = SUM</h2>
+
+      <h2>The difference of number 1 and number 2 = DIFFERENCE</h2>
+
+      <h2>The difference of number 1 and number 2 = DIFFERENCE</h2>
+
+      <h2>The difference of number 1 and number 2 = DIFFERENCE</h2>
+
+      <h2>The product of number 1 and number 2 = PRODUCT</h2>
+
+      <h2>The product of number 1 and number 2 = PRODUCT</h2>
+
+      <h2>The product of number 1 and number 2 = PRODUCT</h2>
+
+      <h2>The quotient of number 1 and number 2 = QUOTIENT</h2>
+
+      <h2>The quotient of number 1 and number 2 = QUOTIENT</h2>
+
+      <h2>The quotient of number 1 and number 2 = QUOTIENT</h2>
+    </>
+
+    )
+}
+export default Mathematics;
